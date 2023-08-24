664140a53ce556fdc7f9c2e8d0e35471f4f748d7
Dan Esparza <danesparza@cagedtornado.com>
2023-08-24 19:09:09 -0400

    Initial commit.  Includes directory structure and start of CI scripts

---
+++ cmd/version.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/danesparza/fxpixel/version"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows the version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		//	Show the version number
+		fmt.Printf("\nfxpixel version %s", version.String())
+
+		//	Show the CommitID if available:
+		if version.CommitID != "" {
+			fmt.Printf(" (%s)", version.CommitID[:7])
+		}
+
+		//	Trailing space and newline
+		fmt.Println(" ")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/danesparza/fxpixel/cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+	"os"
+	"strings"
+	"time"
+)
+
+// @title fxPixel
+// @version 1.0
+// @description fxPixel LED lighting effects REST service
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @BasePath /v1
+func main() {
+	//	Set log info:
+	log.Logger = log.With().Timestamp().Caller().Logger()
+
+	//	Set log level (default to info)
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	switch strings.ToLower(os.Getenv("LOGGER_LEVEL")) {
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+		break
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		break
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		break
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		break
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+		break
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+		break
+	}
+
+	//	Set the error stack marshaller
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	//	Set log time format
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	cmd.Execute()
+}
