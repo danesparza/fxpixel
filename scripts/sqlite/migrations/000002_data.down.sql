d325abfaa25cac354b8a58addf92a4b6ab72440f
Dan Esparza <danesparza@cagedtornado.com>
2023-08-26 15:32:39 -0400

    Add initial lookup table data

---
+++ scripts/sqlite/migrations/000002_data.down.sql
@@ -0,0 +1,2 @@
+delete from timeline_frame_type;
+delete from timeline_scene_type;
---
+++ scripts/sqlite/migrations/000002_data.up.sql
@@ -0,0 +1,9 @@
+/* Initial frame types */
+INSERT INTO timeline_frame_type (id, frame_type) VALUES (1, 'scene');
+INSERT INTO timeline_frame_type (id, frame_type) VALUES (2, 'fade');
+INSERT INTO timeline_frame_type (id, frame_type) VALUES (3, 'sleep');
+
+/* Initial scene types */
+INSERT INTO timeline_scene_type (id, scene_type) VALUES (1, 'solid');
+INSERT INTO timeline_scene_type (id, scene_type) VALUES (2, 'gradient');
+INSERT INTO timeline_scene_type (id, scene_type) VALUES (3, 'sequence');
